8c92b1307783f1df8647d6dc37a255b0d8bec3cd
Marwa Khafagy <marwa.a.khafagy@gmail.com>
2023-03-23 10:58:27 -0400

    i dont remember what I did

--- code/exp1.py
+++ code/exp1.py
@@ -6,4 +6,5 @@ import final_project_part1
     #   the accuracy of the total_dist as the number of k increases?
     #2. given constant k, accuracy of total_dist as graph increases in size
     #3. given constant k and graph size, accuracy of total_dist as graph density increases?
-    #4. compare accuracy of dijskrta to benbell or whatever its called
+    #4. compare accuracy (based on relaxations) of dijskrta to bellman ford or whatever its called
+    #5. see if less runtime if less relaxations??
