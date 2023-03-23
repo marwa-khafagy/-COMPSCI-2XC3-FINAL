929401dc52fc4ce1938c05280e4893888ce7b5a7
Marwa Khafagy <marwa.a.khafagy@gmail.com>
2023-03-23 10:43:04 -0400

    idk

--- code/exp1.py
+++ code/exp1.py
@@ -6,3 +6,4 @@ import final_project_part1
     #   the accuracy of the total_dist as the number of k increases?
     #2. given constant k, accuracy of total_dist as graph increases in size
     #3. given constant k and graph size, accuracy of total_dist as graph density increases?
+    #4. compare accuracy of dijskrta to benbell or whatever its called
