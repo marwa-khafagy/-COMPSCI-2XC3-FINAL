ed22ecded2d30a6fdd23dc03e071463601fb6e48
Alex Eckardt <realalexeck@gmail.com>
2023-03-30 17:40:38 -0400

    Outlined Experiment 1

--- code/exp1.py
+++ code/exp1.py
@@ -4,7 +4,85 @@ import source.final_project_part1
 #experiment ideas
     #1. given random graphs with a constant number of nodes, 
     #   the accuracy of the total_dist as the number of k increases?
+
     #2. given constant k, accuracy of total_dist as graph increases in size
+
     #3. given constant k and graph size, accuracy of total_dist as graph density increases?
+
     #4. compare accuracy (based on relaxations) of dijskrta to bellman ford or whatever its called
+    
     #5. see if less runtime if less relaxations??
+
+#
+# ===================================================================================
+#
+
+def test1():
+    pass
+
+
+#
+# ===================================================================================
+#
+
+def test2():
+    pass
+
+
+#
+# ===================================================================================
+#
+
+def test3():
+    pass
+
+
+#
+# ===================================================================================
+#
+
+def test4():
+    pass
+
+
+#
+# ===================================================================================
+#
+
+def test5():
+    pass
+
+#
+# ===================================================================================
+#
+
+if (__name__ == "__main__"):
+
+    #Run Tests
+
+    #
+    # Test 1
+    #
+
+
+    #
+    # Test 2
+    #
+
+
+    #
+    # Test 3
+    #
+
+
+    #
+    # Test 4
+    #
+
+
+    #
+    # Test 5
+    #
+
+    #End
+    pass
