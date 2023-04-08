49005326c50ff57a261bdb0f47ec35b9d60cb5b1
Marwa Khafagy <marwa.a.khafagy@gmail.com>
2023-04-08 17:54:04 -0400

    now actually done refactoring

--- code/part4/short_path_finder.py
+++ code/part4/short_path_finder.py
@@ -1,3 +1,14 @@
 
 class ShortPathFinder:
-    pass
+    def __init__(self):
+        self.graph = None
+        self.alg = None
+    
+    def set_graph(self, graph):
+        self.graph = graph
+    
+    def set_algorithm(self, alg):
+        self.alg = alg
+    
+    def calc_short_path(self, source, dest):
+        return self.alg.calc_sp(self.graph, source, dest)
