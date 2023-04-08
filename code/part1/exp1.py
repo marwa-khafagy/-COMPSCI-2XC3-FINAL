0d79e70a5450a2bd8dc834512b9bd67db209ba3a
Alex Eckardt <realalexeck@gmail.com>
2023-04-08 15:28:24 -0400

    Automated the saving of Figures in Experiment 1

--- code/part1/exp1.py
+++ code/part1/exp1.py
@@ -5,6 +5,12 @@ from plotting import PlotGroup
 import shortest_path_approx
 import matplotlib.pyplot as plot
 import source.final_project_part1 as fpp1
+import os
+
+saveFileDir = "./images/part1/"
+token = 0
+saveFigures = True
+showFigures = False
 
 #experiment ideas
 
@@ -15,6 +21,17 @@ import source.final_project_part1 as fpp1
     
     #5. see if less runtime if less relaxations?
 
+def finish_figure(saveLocation):
+
+    if saveFigures:
+        print("Saving Figure to " + saveLocation)
+        plot.savefig(saveFileDir + saveLocation, bbox_inches='tight')
+
+    if showFigures:
+        plot.show() 
+
+    #Reset
+    plot.close() 
 
 def create_random_complete_graph(n,lower,upper):
     G = fpp1.DirectedWeightedGraph()
@@ -35,14 +52,16 @@ def create_random_complete_graph(n,lower,upper):
 #   the accuracy of the total_dist as the number of k increases, from EVERY node?
 
 #On Dijkstas
-def test1_d(nodeCount, lowerWeight, upperWeight, relaxationRange):
-    return test1(nodeCount, lowerWeight, upperWeight, relaxationRange, dijkstra, dijkstra_approx, "Dijkstra")
+def test1_d(nodeCount, lowerWeight, upperWeight, relaxationRange, token):
+    return test1(nodeCount, lowerWeight, upperWeight, relaxationRange, dijkstra, dijkstra_approx, token, "Dijkstra")
 
-def test1_bf(nodeCount, lowerWeight, upperWeight, relaxationRange):
-    return test1(nodeCount, lowerWeight, upperWeight, relaxationRange, bellman_ford, bellman_ford_approx, "Bellman Ford")
+def test1_bf(nodeCount, lowerWeight, upperWeight, relaxationRange, token):
+    return test1(nodeCount, lowerWeight, upperWeight, relaxationRange, bellman_ford, bellman_ford_approx, token, "Bellman-Ford")
 
-def test1(nodeCount, lowerWeight, upperWeight, relaxationRange, realfunc, approxfunc, alg="Unknown"):
+def test1(nodeCount, lowerWeight, upperWeight, relaxationRange, realfunc, approxfunc, token, alg="Unknown"):
     
+    print(f'Starting Test 1{token}...')
+
     aplot = PlotGroup(f"{alg} Approximation Accuracy")
 
     realPlot = PlotGroup(f"{alg} Algorithm Total Distance")
@@ -72,16 +91,20 @@ def test1(nodeCount, lowerWeight, upperWeight, relaxationRange, realfunc, approx
         approxPlot.add_point(relaxAmount, approxDist);
 
     #Setup
-    plot.title(f"Accuracy of the Total Distance of {alg} Real vs {alg} Approx with x relaxations")
+    plot.title(f"Difference in Total Distance of {alg} vs {alg} Approx On Graph of {nodeCount} Nodes")
     plot.xlabel(f"Number of Relaxations in the Approximation Function")
     plot.ylabel("Accuracy of Approximate Distance to Total Distance")
     
     aplot.plot()
 
     plot.legend()
-    plot.show()
+    finish_figure(f'exp1_1{token}A.png')
+  
+    #
+    # Second Graph
+    #
 
-    plot.title(f"Real Total Distance of {alg} Real vs {alg} Approx with X relaxations")
+    plot.title(f"Total Distances of {alg} vs {alg} Approx On Graph of {nodeCount} Nodes")
     plot.xlabel(f"Number of Relaxations in the Approximation Function")
     plot.ylabel("Accuracy of Approximate Distance to Total Distance")
     
@@ -89,7 +112,7 @@ def test1(nodeCount, lowerWeight, upperWeight, relaxationRange, realfunc, approx
     approxPlot.plot()
 
     plot.legend()
-    plot.show()
+    finish_figure(f'exp1_1{token}B.png')
 
 
 # ===================================================================================
@@ -97,7 +120,9 @@ def test1(nodeCount, lowerWeight, upperWeight, relaxationRange, realfunc, approx
 
 #
 #As relaxations increase, how does the accuracy fair from some single starting node?
-def test4(nodeCount, lowerWeight, upperWeight, relaxationRange, startingNode, trials):
+def test4(nodeCount, lowerWeight, upperWeight, relaxationRange, startingNode, trials, token):
+
+    print(f'Starting Test 4{token}...')
 
     dplot = PlotGroup(f"Dijkstra Approximation Accuracy")
     bfplot = PlotGroup(f"Bellmand Ford Approximation Accuracy")
@@ -140,7 +165,8 @@ def test4(nodeCount, lowerWeight, upperWeight, relaxationRange, startingNode, tr
     plot.xlabel(f"Number of Relaxations in the Approximation Function")
     plot.ylabel("Accuracy of Approximate Distance to Total Distance")    
     plot.legend()
-    plot.show()
+
+    finish_figure(f'exp1_4{token}.png')
 
 
 #
@@ -151,7 +177,9 @@ def test4(nodeCount, lowerWeight, upperWeight, relaxationRange, startingNode, tr
 
 #NOTE: Many trials needed as graphs are random. However, we perform the same alg on the same graph for each trial,
 #so the averages are okay. We average to get a general trend as items can vary.
-def test5(nodeRange, upperWeight, constantRelaxations, startingNode, trials):
+def test5(nodeRange, upperWeight, constantRelaxations, startingNode, trials, token):
+
+    print(f'Starting Test 5{token}...')
 
     approxPlots = []
 
@@ -195,7 +223,8 @@ def test5(nodeRange, upperWeight, constantRelaxations, startingNode, trials):
     plot.xlabel(f"Number of Nodes in graph G")
     plot.ylabel("Total Distance of graph")    
     plot.legend()
-    plot.show()
+
+    finish_figure(f'exp1_5{token}.png')
 
     pass
 
@@ -207,26 +236,26 @@ if (__name__ == "__main__"):
 
     #Run Tests
 
+    token = 0
+
     #
     # Test 1
     #
-    
-    test1_d(50, 0, 100, range(50 + 1))
-    test1_bf(25, 0, 100, range(1, 25 + 1))
-    test1_bf(25, -100, 100, range(1, 25 + 1))
+    # test1_d(50, 0, 100, range(1, 50 + 1), 'a')
+    # test1_bf(25, 0, 100, range(1, 25 + 1), 'b')
+    # test1_bf(25, -100, 100, range(1, 25 + 1), 'c')
+    # test1_bf(25, -100, 100, range(1, 1000, 10), 'd')
 
-    test1_bf(25, -100, 100, range(1, 1000, 10))
     #
     # Test 4
     #
-
-    # test4(50, 0, 100, range(50), 0, 1)
-    # test4(50, 0, 100, range(50), 0, 1000)
+    test4(50, 0, 100, range(50), 0, 1, 'a')
+    test4(50, 0, 100, range(50), 0, 1000, 'b')
 
     #
     # Test 5
     #
-    # test5(range(10, 100, 10), 100, [1, 10, 20, 50, 100], 0, 1000)
+    test5(range(10, 100, 10), 100, [1, 10, 20, 50, 100], 0, 1000, 'a')
 
 
     #End
