a8099b59130a54f5fbc307910ff2d10aecb49052
Marwa Khafagy <marwa.a.khafagy@gmail.com>
2023-04-16 23:16:36 -0400

    last min

--- code/part4/A_star.py
+++ code/part4/A_star.py
@@ -14,7 +14,7 @@ class A_star(SPAlgorithm):
         else:
             h = graph.get_heuristic()
 
-        return self.calc_sp_aux(graph, source, dest, graph.get_heuristic())
+        return self.calc_sp_aux(graph, source, dest, h)
         
     def calc_sp_aux(self,G, source, dest, h):
         pred = {} #Predecessor dictionary
